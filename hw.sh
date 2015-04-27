bcf623990372f2828db4fa827035780b92aad6b9
Fabrizio <fab@lungo.co.uk>
2015-04-27 15:11:47 +0100

    Added code to generate hw.h

---
+++ hw.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+. c.sh
+
+FILE=hw.h
+MAP=hw.map
+
+init $FILE
+
+map_line() {
+  local ALIAS=$(echo $1 | cut -d'-' -f 1 | awk '{$1=$1}{ print }')
+  local NAME=$(echo $1 | cut -d'>' -f 2 | awk '{$1=$1}{ print }')
+  local PORT=${NAME:1:1}
+  local PIN=${NAME:2:1}
+  echo "#define ${ALIAS}_PORT PORT${PORT}"
+  echo "#define ${ALIAS}_DDR  DDR${PORT}"
+  echo "#define ${ALIAS}_PIN  P${PORT}${PIN}"
+  echo "#define ${ALIAS}      ${ALIAS}_PORT, ${ALIAS}_DDR, ${ALIAS}_PIN"
+}
+
+map_hw() {
+  cat $MAP | while read line; do
+    case $line in
+      "##"*)
+        header ${line:2}
+      ;;
+      "#include"*|"#define"*)
+        echo $line
+      ;;
+      "# "*)
+        comment ${line:2}
+      ;;
+      *"->"*)
+        map_line "$line"
+      ;;
+      "")
+        echo
+      ;;
+    esac
+  done
+}
+
+map_hw > hw.h
