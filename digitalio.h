24dcfde95d6a24ece0b1dc9d34c7849b7d61e1ae
Fabrizio <fab@lungo.co.uk>
2015-05-08 06:17:59 +0100

    Moved macros to digitalio.h

---
+++ digitalio.h
@@ -0,0 +1,13 @@
+/* Mode setting macros */
+#define OUTPUT_(port, ddr, pin)        ddr |= 1 << pin
+#define OUTPUT(spec)                   OUTPUT_(spec)
+#define INPUT_(port, ddr, pin)         ddr &= ~(1 << pin)
+#define INPUT(spec)                    INPUT_(spec)
+
+/* Digital write macros (also used to toggle pull up resistor on inputs) */
+#define SET_(port, ddr, pin)           port |= 1 << pin
+#define SET(spec)                      SET_(spec)
+#define CLEAR_(port, ddr, pin)         port &= ~(1 << pin)
+#define CLEAR(spec)                    CLEAR_(spec)
+#define TOGGLE_(port, ddr, pin)        port ^= 1 << pin
+#define TOGGLE(spec)                   TOGGLE_(spec)
